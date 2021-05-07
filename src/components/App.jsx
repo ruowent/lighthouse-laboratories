f3aef415a85b5b7b3802611cf98d46470a045361
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-07 16:34:21 -0400

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Nav from "./Nav";
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div style={{ textAlign: "center" }}>
+				<Nav />
 				<h1>Hello World</h1>
 			</div>
 		);
